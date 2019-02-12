7fe4827c1d00467857413133a801a265fc736df4
wuriyanto <wuriyanto48@yahoo.co.id>
2019-02-12 10:49:03 +0700

    chore: validate input before create map variable from input

--- json_hide.go
+++ json_hide.go
@@ -1,6 +1,13 @@
 package goson
 
+import (
+	"errors"
+	"reflect"
+)
+
 // this utility will hide json response based on struct json field you choose
+
+var errorsShouldBeStruct = errors.New("input should be struct")
 
 type fieldSet map[string]bool
 
@@ -17,5 +24,30 @@ func genFieldSet(fields ...string) fieldSet {
 // - input (struct only)
 // - fields json field that you will be hide
 func HideFields(input interface{}, fields ...string) (map[string]interface{}, error) {
-	return nil, nil
+	typeInput := reflect.TypeOf(input)
+
+	// validate input
+	// if input is not struct return early errors
+	if typeInput.Kind() != reflect.Struct {
+		return nil, errorsShouldBeStruct
+	}
+
+	fieldSet := genFieldSet(fields...)
+
+	output := make(map[string]interface{}, typeInput.NumField())
+
+	valueInput := reflect.ValueOf(input)
+
+	for i := 0; i < typeInput.NumField(); i++ {
+		// field return each file on the struct
+		field := typeInput.Field(i)
+
+		// get json Tag
+		// eg: Name `json:"name"` => result will be `name`
+		jsonField := field.Tag.Get("json")
+		if !fieldSet[jsonField] {
+			output[jsonField] = valueInput.Field(i).Interface()
+		}
+	}
+	return output, nil
 }
