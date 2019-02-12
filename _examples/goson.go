98d3a69c44a1b656e759d309aace201d372afd92
wuriyanto <wuriyanto48@yahoo.co.id>
2019-02-12 11:14:20 +0700

    add examples

---
+++ _examples/goson.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/Bhinneka/goson"
+)
+
+// Bhinnekaners type
+type Bhinnekaners struct {
+	ID        string    `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	BirthDate time.Time `json:"birthDate"`
+	Created   time.Time `json:"created"`
+	CreatorIP string    `json:"creatorIp"`
+}
+
+func main() {
+	wury := &Bhinnekaners{
+		ID:        "001",
+		FirstName: "Wuriyanto",
+		LastName:  "Musobar",
+		BirthDate: time.Now(),
+		Created:   time.Now(),
+		CreatorIP: "10.0.1.1",
+	}
+
+	b1, err := json.Marshal(wury)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(b1))
+
+	result, err := goson.HideFields(*wury, "created", "creatorIp")
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	b2, err := json.Marshal(result)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(b2))
+}
