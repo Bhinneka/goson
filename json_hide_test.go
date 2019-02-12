cdc0de43cc35aaec17b6286dc1d9369b1e367572
wuriyanto <wuriyanto48@yahoo.co.id>
2019-02-12 10:49:22 +0700

    fix code for pass the test

--- json_hide_test.go
+++ json_hide_test.go
@@ -1,6 +1,7 @@
 package goson
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 )
@@ -18,11 +19,14 @@ type bhinnekaners struct {
 
 func TestJsonHide(t *testing.T) {
 
+	birthDateLayout := "02/01/2006"
+	birthDate, _ := time.Parse(birthDateLayout, "01/01/2019")
+
 	wury := bhinnekaners{
 		ID:        "001",
 		FirstName: "Wuriyanto",
 		LastName:  "Musobar",
-		BirthDate: time.Now(),
+		BirthDate: birthDate,
 		Created:   time.Now(),
 		CreatorIP: "10.0.1.1",
 	}
@@ -36,6 +40,35 @@ func TestJsonHide(t *testing.T) {
 
 		if result == nil {
 			t.Error("error hiding json fields")
+		}
+
+		if result != nil {
+			t.Log(result)
+		}
+
+		expectedOutput := `{"birthDate":"2019-01-01T00:00:00Z","firstName":"Wuriyanto","id":"001","lastName":"Musobar"}`
+
+		b, _ := json.Marshal(result)
+
+		eq := string(b) == expectedOutput
+
+		t.Log(string(b))
+
+		if !eq {
+			t.Error("result is not equal expectedOutput")
+		}
+
+	})
+
+	t.Run("should return error if input is not struct", func(t *testing.T) {
+		result, err := HideFields("", "created", "creatorIp")
+
+		if err == nil {
+			t.Error("should error, input is not struct")
+		}
+
+		if result != nil {
+			t.Error("should error, input is not struct")
 		}
 	})
 }
