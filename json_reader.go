1b08e670a27f33f375e0ea30a373ea840023b62d
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2019-07-15 17:23:58 +0700

    update: json reader

--- json_reader.go
+++ json_reader.go
@@ -2,6 +2,10 @@ package goson
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 var (
@@ -17,22 +21,25 @@ type Reader interface {
 
 // Field from json reader
 type Field interface {
-	Uint() uint
-	Int() int
-	String() string
-	Float() float64
-	Bool() bool
+	Uint() (uint, error)
+	Int() (int, error)
+	String() (string, error)
+	Float() (float64, error)
+	Bool() (bool, error)
 	SetTo(target interface{}) error
 }
 
 // Read from json input
-func Read(data []byte) Reader {
+func Read(data []byte) (Reader, error) {
 	var tmp interface{}
-	json.Unmarshal(data, &tmp)
+	err := json.Unmarshal(data, &tmp)
+	if err != nil {
+		return nil, err
+	}
 
 	return &reader{
 		data: tmp,
-	}
+	}, nil
 }
 
 type reader struct {
@@ -40,29 +47,73 @@ type reader struct {
 }
 
 func (r *reader) GetField(keys ...string) Field {
-	return nil
+	var data interface{} = r.data
+	field := new(field)
+	func() {
+		var traces []string
+		defer func() {
+			if r := recover(); r != nil {
+				field.err = fmt.Errorf("key '%s' not found or out of range from JSON data", strings.Join(traces, " => "))
+			}
+		}()
+		for _, k := range keys {
+			traces = append(traces, k)
+			if arrayCheck[k[0]] == k[len(k)-1] {
+				if idx, err := strconv.Atoi(k[1 : len(k)-1]); err == nil {
+					data = data.([]interface{})[idx]
+				}
+				continue
+			}
+			tmp, ok := data.(map[string]interface{})[k]
+			if !ok && tmp == nil {
+				panic("invalid key")
+			}
+			data = tmp
+		}
+	}()
+
+	field.value = data
+	return field
 }
 
 type field struct {
+	err   error
 	value interface{}
 }
 
-func (f *field) Uint() uint {
-	return 0
+func (f *field) set(v reflect.Value) error {
+	if f.err != nil {
+		return f.err
+	}
+	return setValue(v, f.value)
 }
 
-func (f *field) Int() int {
-	return 0
+func (f *field) Uint() (u uint, err error) {
+	err = f.set(reflect.ValueOf(&u))
+	return
 }
 
-func (f *field) Float() float64 {
-	return 0.0
+func (f *field) Int() (i int, err error) {
+	err = f.set(reflect.ValueOf(&i))
+	return
 }
 
-func (f *field) String() string {
-	return ""
+func (f *field) Float() (fl float64, err error) {
+	err = f.set(reflect.ValueOf(&fl))
+	return
 }
 
-func (f *field) SetTo(target interface{}) error {
-	return nil
+func (f *field) String() (s string, err error) {
+	err = f.set(reflect.ValueOf(&s))
+	return
+}
+
+func (f *field) Bool() (b bool, err error) {
+	err = f.set(reflect.ValueOf(&b))
+	return
+}
+
+func (f *field) SetTo(target interface{}) (err error) {
+	err = f.set(reflect.ValueOf(target))
+	return
 }
---
+++ json_reader_test.go
@@ -0,0 +1,104 @@
+package goson
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Reader(t *testing.T) {
+	jsonStr := []byte(`{
+		"id": "0101",
+		"name": "agungdp",
+		"mustFloat": "2.23423",
+		"mustInt": 2.23423,
+		"uint": 11,
+		"nullable": null,
+		"isExist": "true",
+		"obj": {
+		  "n": 2,
+		  "testing": {
+			"ss": "23840923849284"
+		  }
+		},
+		"slice": [
+		  {
+			"fieldA": "100",
+			"fieldB": 3000,
+			"exist": true,
+			"test": "3.14"
+		  },
+		  {
+			"fieldA": 50000,
+			"fieldB": "123000",
+			"exist": 3000,
+			"test": 1323.123
+		  }
+		],
+		"str": ["a", true],
+		"ints": ["2", 3],
+		"bools": [1, "true", "0", true],
+		"NoTag": 19283091832
+	  }`)
+
+	reader, err := Read(jsonStr)
+	assert.NoError(t, err)
+
+	t.Run("Testcase #1: Get string field", func(t *testing.T) {
+		str, err := reader.GetField("slice", "[1]", "fieldA").String()
+		assert.NoError(t, err)
+		assert.Equal(t, str, "50000")
+
+		str, err = reader.GetField("slice", "[5]", "fieldA").String()
+		assert.Error(t, err)
+	})
+
+	t.Run("Testcase #2: Get int field", func(t *testing.T) {
+		i, err := reader.GetField("bools", "[3]").Int()
+		assert.NoError(t, err)
+		assert.Equal(t, i, 1)
+
+		i, err = reader.GetField("obj", "testing", "ssa").Int()
+		assert.Error(t, err)
+	})
+
+	t.Run("Testcase #3: Get float field", func(t *testing.T) {
+		fl, err := reader.GetField("slice", "[0]", "test").Float()
+		assert.NoError(t, err)
+		assert.Equal(t, fl, 3.14)
+
+		fl, err = reader.GetField("slice", "[0]", "a").Float()
+		assert.Error(t, err)
+	})
+
+	t.Run("Testcase #4: Get uint field", func(t *testing.T) {
+		u, err := reader.GetField("uint").Uint()
+		assert.NoError(t, err)
+		assert.Equal(t, u, uint(11))
+
+		u, err = reader.GetField("slice", "a", "b").Uint()
+		assert.Error(t, err)
+	})
+
+	t.Run("Testcase #5: Get bool field", func(t *testing.T) {
+		b, err := reader.GetField("isExist").Bool()
+		assert.NoError(t, err)
+		assert.Equal(t, b, true)
+
+		b, err = reader.GetField("slice", "a", "b").Bool()
+		assert.Error(t, err)
+	})
+
+	t.Run("Testcase #6: Set to model", func(t *testing.T) {
+		var m struct {
+			SS string `json:"ss"`
+		}
+
+		err := reader.GetField("obj", "testing").SetTo(&m)
+		assert.NoError(t, err)
+		assert.Equal(t, m.SS, "23840923849284")
+
+		err = reader.GetField("slice", "a", "b").SetTo(&m)
+		assert.Error(t, err)
+	})
+}
